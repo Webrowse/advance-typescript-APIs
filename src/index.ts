5ba54ca1a9495c7a8cb14f8e9c03d438fcbb396c
Webrowse <great.adarsh@gmail.com>
2025-01-10 15:08:39 +0530

    readonly
    the content of an array or object cannot be changed

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
-interface User{
-    id: string;
-    name: string;
-    age: number;
+// interface User{
+//     id: string;
+//     name: string;
+//     age: number;
+//     email: string;
+//     password: string;
+// }
+
+// // Pick let you choose a subtype of User
+// type UpdateProps = Pick<User, 'name' | 'age' | 'email'>
+
+
+// //Partial makes each field optional
+// function updatedUser(updatedProps: Partial<UpdateProps>){
+//     return {...updatedProps};
+// }
+
+// const result = updatedUser({name: 'John', age:12});
+// console.log(result);
+
+//Readonly properties
+type User = {
+    readonly name: string;
     email: string;
-    password: string;
 }
 
-// Pick let you choose a subtype of User
-type UpdateProps = Pick<User, 'name' | 'age' | 'email'>
-
-
-//Partial makes each field optional
-function updatedUser(updatedProps: Partial<UpdateProps>){
-    return {...updatedProps};
+const user: User = {
+    name: 'Adarsh',
+    email: 'old@email.com'
 }
 
-const result = updatedUser({name: 'John', age:12});
-console.log(result);
+user.name = 'John'; //not allowed as it is mentioned Read-only
+user.email = 'new@email.com'; // this is allowed

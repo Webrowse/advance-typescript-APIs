0d0aaf31711e9f61fa5c1c005f9a456a967bc6d6
Webrowse <great.adarsh@gmail.com>
2025-01-10 22:14:22 +0530

    records in ts

--- src/index.ts
+++ src/index.ts
@@ -19,15 +19,24 @@
 // console.log(result);
 
 //Readonly properties
-type User = {
-    readonly name: string;
-    email: string;
-}
+// type User = {
+//     readonly name: string;
+//     email: string;
+// }
 
-const user: User = {
-    name: 'Adarsh',
-    email: 'old@email.com'
-}
+// const user: User = { // you can write Readonly<User>
+//     name: 'Adarsh',
+//     email: 'old@email.com'
+// }
 
-user.name = 'John'; //not allowed as it is mentioned Read-only
-user.email = 'new@email.com'; // this is allowed
+// user.name = 'John'; //not allowed as it is mentioned Read-only
+// user.email = 'new@email.com'; // this is allowed
+
+// =========Records =================
+type Users = Record<string, {age:number, name:string}>;
+
+const user:Users = {
+    '1': {age: 25, name: 'John'},
+    '2': {age: 30, name: 'Jane'},
+    '3': {age: 28, name: 'Mike'},
+}
